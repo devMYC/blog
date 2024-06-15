97aeaeb63fac2bace61769aeaa703af7c3bbaad4
Martin <mycha0@hotmail.com>
2024-06-15 12:01:01 -0700

    test

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 // import blog from 'https://deno.land/x/blog@0.3.3/blog.tsx';
 // import blog from 'https://raw.githubusercontent.com/devMYC/deno_blog/87b4744bf5aac47e10b52c88437d0e08715821fb/blog.tsx';
-import blog from 'https://github.com/devMYC/deno_blog/blob/fe96ae2023c46fdf7491b158cc70e7a4c3e57bb9/blog.tsx';
+import blog from 'https://raw.githubusercontent.com/devMYC/deno_blog/fe96ae2023c46fdf7491b158cc70e7a4c3e57bb9/blog.tsx';
 
 blog({
     title: 'Yichao Ma',
