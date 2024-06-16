5ba0f6499377c66ecdca6b28216cef90fec1183e
Yichao <mycha0@hotmail.com>
2024-06-15 22:11:39 -0700

    Link to CV (#3)

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 // import blog from 'https://deno.land/x/blog@0.3.3/blog.tsx';
-import blog from 'https://raw.githubusercontent.com/devMYC/deno_blog/87b4744bf5aac47e10b52c88437d0e08715821fb/blog.tsx';
+import blog from 'https://raw.githubusercontent.com/devMYC/deno_blog/e4aca2327af577f63a86cf2aa6aefb7c35956634/blog.tsx';
 
 blog({
     title: 'Yichao Ma',
